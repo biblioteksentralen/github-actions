09af0723ae2dac77d393e03534de81345b97f4da
Dan Michael O. Heggø <dan.michael.heggo@bibsent.no>
2024-07-16 09:30:05 +0200

    notify-teams: split main functions into smaller functions

--- notify-teams/index.mjs
+++ notify-teams/index.mjs
@@ -1,12 +1,37 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 
+/**
+ * @typedef {Object} CardData
+ * @property {string} title
+ * @property {string} text
+ * @property {string|undefined} logUrl
+ */
+
 async function main() {
   const webhookUrl = core.getInput("webhooks-url");
   const title = core.getInput("title");
   const text = core.getInput("text");
   const githubToken = core.getInput("github-token");
 
+  const job = await fetchJobData(githubToken);
+  const logUrl = job?.html_url ?? undefined;
+
+  /**
+   * @type {CardData}
+   */
+  const cardData = { title, text, logUrl };
+  const card = formatCard(cardData);
+  const [err] = await sendPostRequest(webhookUrl, card);
+  if (err) {
+    console.error(`Failed to send notification: ${err.message}`);
+  }
+}
+
+/**
+ * @param {string} githubToken
+ */
+async function fetchJobData(githubToken) {
   const octokit = github.getOctokit(githubToken);
   const { context } = github;
   console.log(`GITHUB_RUN_ATTEMPT: ${process.env.GITHUB_RUN_ATTEMPT}`);
@@ -16,26 +41,30 @@ async function main() {
     attempt_number: parseInt(process.env.GITHUB_RUN_ATTEMPT ?? "1"),
   });
   const job = data.jobs[0];
-  console.log(job);
+  console.debug(job);
+  return job;
+}
 
-  const logUrl = job?.html_url;
-  const postData = JSON.stringify({
+/**
+ * @param {CardData} props
+ * @returns {string}
+ */
+function formatCard(props) {
+  const actions = [];
+  if (props.logUrl) {
+    actions.push({
+      "@type": "OpenUri",
+      name: "Vis logg",
+      targets: [{ os: "default", uri: props.logUrl }],
+    });
+  }
+  return JSON.stringify({
     "@type": "MessageCard",
     "@context": "https://schema.org/extensions",
-    title,
-    text,
-    potentialAction: [
-      {
-        "@type": "OpenUri",
-        name: "Vis logg",
-        targets: [{ os: "default", uri: logUrl }],
-      },
-    ],
+    title: props.title,
+    text: props.text,
+    potentialAction: actions,
   });
-  const [err] = await sendPostRequest(webhookUrl, postData);
-  if (err) {
-    console.error(`Failed to send notification: ${err.message}`);
-  }
 }
 
 /**
